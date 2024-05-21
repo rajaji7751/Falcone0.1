03a7e363ec27eaad487a67011f3bba6c84e78ca6
rajaji7751 <kumarsahoo7751@gmail.com>
2024-05-21 16:34:57 +0530

    new file add

---
+++ project 2/strings.js
@@ -0,0 +1,48 @@
+// let name ="Rajesh kumar sahoo"
+// console.log(name.length)
+// console.log(name[0])
+// console.log(name[2])
+// let friend ='Saswat'
+
+// Template literals
+// let boy1="Rajesh"
+// let boy2="Saswat"
+//Saswat is a friend of Rajesh
+// let sentence =`${boy2} is a friend of ${boy1}`
+// console.log(sentence)
+
+
+// Escape Sequence Characters
+// let fruit ='Bana\'na'
+// console.log(fruit)
+// let fruit = "Bana\"na"
+// console.log(fruit)
+
+let name = "Rajesh"
+// console.log(name.length)
+
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+// console.log(name.slice(2,4))
+// console.log(name.slice(2))
+// console.log(name.replace("Raj","oos"))
+
+// let friend ="Saswat"
+// console.log(name.concat(" is a friend of ", friend, "Ok"))
+// let friend2 ="      Sanjana      "
+// console.log(friend2)
+// console.log(friend2.trim())/
+
+
+// let fr ="Katrina"
+// console.log(fr[0])
+// console.log(fr[1])
+// console.log(fr[2])
+// console.log(fr[3])
+
+let fr= "katrina" + "salman"
+console.log(fr[0])
+console.log(fr[1])
+console.log(fr[2])
+console.log(fr[3])
+console.log(fr)
