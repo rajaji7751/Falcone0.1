791520b5a69c5040e8a61a6af132fe42db51df43
rajaji7751 <kumarsahoo7751@gmail.com>
2024-08-09 17:50:23 +0530

    new

---
+++ class3/data.js
@@ -0,0 +1,22 @@
+// Dates
+
+let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.getTime());
+// console.log(myDate.getMonth());
+// console.log(myDate.getFullYear());
+// console.log(myDate.getMilliseconds());
+// console.log(myDate.getSeconds());
+
+// todo ? print your birthdaynin the console using the data object.
+// todo ? print the current time in the console using the data object.
+
+
+
+  let MyBdy = new Date("2001-11-27");
+  console.log(MyBdy);
+
+  
---
+++ class4/ans.js
@@ -0,0 +1,110 @@
+// let today = newDate();
+// console.log(today);
+
+// let newData = newDate("2001-11-27");
+// console .log(MyBdy);
+
+// let MyDate = new Date();
+// console.log(MyDate.toString());
+// console.log(MyDate.toDateString());
+// console.log(MyDate.toLocaleString());
+
+
+// let myCreatedDate = new Date(2001,11,27)
+// console.log(myCreatedDate.toDateString());
+
+// let number =[1,2,3];
+// number.push(4);
+// number.push(5);
+// console.log(number);
+
+// let number =[1,2,3,4,5];
+// number.pop();
+// number.pop();
+// console.log(number);
+
+// let lastItem =number.pop();
+// console.log(lastItem);
+
+// Given the array let fruits = ["apple", "banana", "mango"];, what will be the result of fruits.push("orange")
+
+// let fruits =["apple","banana","mango",];
+// console.log(fruits);
+
+// fruits.push("orange");
+// console.log(fruits);
+
+
+// What will the following code output?
+// let colors = ["red", "green", "blue"];
+// colors.push("yellow");
+// colors.pop();
+// console.log(colors);
+
+// let colors=["red","green","blue"];
+// console.log(colors);
+// colors.push("yellow");
+// console.log(colors);
+// colors.pop();
+// colors.pop();
+// console.log(colors);
+
+
+// How can you add multiple elements to the end of an array in one push call
+
+// let names =["koli","rohit","rahul"]
+// names.push("thakur","surya")
+// console.log(names);
+
+// What will be the result of the following code?
+// let stack = [];
+// stack.push(1);
+// stack.push(2);
+// stack.push(3);
+// stack.pop();
+// stack.pop();
+// console.log(stack);
+
+
+// let stack =[];
+// stack.push(1);
+// stack.push(2);
+// stack.push(3);
+// stack.pop();
+// stack.pop();
+// console.log(stack);
+
+// Ans-[1]
+
+
+// Explain the following code:
+// let letters = ["a", "b"];
+// letters.push("c", "d");
+// let lastLetter = letters.pop();
+// console.log(letters);
+// console.log(lastLetter);
+
+let letters=["a","b"];
+letters.push("c","d");
+// console.log(letters); // Ans-9['a','b','c','d']
+let lastLetter = letters.pop();
+// console.log(lastLetter); Ans-d 
+
+
+// What will be the output of the following code?
+// let myArray = [10, 20, 30];
+// myArray.push(40);
+// console.log(myArray.pop());
+// console.log(myArray);
+// ``` -->
+
+let myArray=[10,20,30];
+myArray.push(40);
+console.log(myArray.pop());
+console.log(myArray);
+
+Ans-40
+// [ 10, 20, 30 ]
+
+
+
